41e6455ffc72b7599028668455ddc16ebfc09bbe
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-10-08 19:19:45 +0900

    変換処理

--- app/src/main/java/com/example/unitconverterapp/MainActivity.java
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.unitconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultLabel;
+    EditText editText;
+    Button resultButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultLabel = findViewById(R.id.textView);
+        editText = findViewById(R.id.editTextText);
+        resultButton = findViewById(R.id.button);
+
+        editText.setText("");
+
+        resultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String inputText = editText.getText().toString();
+
+                double kilo = Double.parseDouble(inputText);
+
+                double pounds = conversion(kilo);
+
+                resultLabel.setText("" + pounds);
+            }
+        });
+
+    }
+
+    double conversion(double kilo) {
+        return kilo * 2.20462;
+
     }
 }
